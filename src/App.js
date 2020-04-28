98cff8503ba8686845895f4954009caacffdc805
danj-O <danjowanjo@gmail.com>
2020-04-28 12:59:00 -0400

    implemented fb utils, included ability to store authd users in fs db

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component'
 import Header from './components/header/header.component'
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 class App extends React.Component {
   constructor() {
@@ -18,10 +18,32 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      //when user signs in, check if they are signing in
+      if (userAuth) {
+        //get the userRef from createUserProfileDocument which is a function in firabase.utils
+        const userRef = await createUserProfileDocument(userAuth)
+        //get the snapshot of the userAuth
+        userRef.onSnapshot(snapshot => {
+          //create an object that combines id from snapshot and all the other data.  id is not found in snapshot.data
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          }, 
+          //because setstate asyncronous, we need to console.log as another param to setstate, so we are waiting for the state to be set before logging
+          () => {
+            console.log(this.state)
 
-      console.log(user)
+          })
+        })
+      } else {
+        //otherwise, this will set the user's state to null, which we get back from the auth library
+        this.setState({ currentUser: userAuth })
+        console.log(this.state)
+
+      }
     })
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
-//object from fribase website
+//object from firebase website
 const config = {
   apiKey: "AIzaSyBjBr3C41uKHoZA9N2k6Kmw3dSs_WBI6wQ",
   authDomain: "ztm-ecommerce.firebaseapp.com",
@@ -12,6 +12,37 @@ const config = {
   messagingSenderId: "435269420717",
   appId: "1:435269420717:web:5a05323097c3e2c50da824",
   measurementId: "G-X5WBQMTJ9G"
+}
+
+//userAuth is the massive object coming from google containing all users info
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  //query to see if the uid already exists
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+  //get the snapshot of the user data from google
+  const snapshot = await userRef.get()
+
+  //exists is a property of the snapshot object. if the snapshot doesnt exist, create one in the try catch
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth
+    const createdAt = new Date()
+
+    //async req to store the data in the firebase DB
+    try {
+      //.set is to create object - a new user object gets stored in the db with set
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message)
+    }
+  }
+  //return userRef in case we want to use this data somewhere else
+  // console.log(userRef)
+  return userRef
 }
 
 firebase.initializeApp(config)
