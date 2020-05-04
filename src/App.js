e08714fa0a10e9f5cb767fe5c17dc39fa25f186f
danj-O <danjowanjo@gmail.com>
2020-05-04 15:08:26 -0400

    added redux and implemented userreducer and useractions to header and app comp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom'
+import { connect } from 'react-redux'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component'
 import Header from './components/header/header.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import { setCurrentUser } from './redux/user/user.actions'
 
 class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null
 
   componentDidMount() {
+    const {setCurrentUser} = this.props
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       //when user signs in, check if they are signing in
       if (userAuth) {
@@ -26,21 +24,14 @@ class App extends React.Component {
         //get the snapshot of the userAuth
         userRef.onSnapshot(snapshot => {
           //create an object that combines id from snapshot and all the other data.  id is not found in snapshot.data
-          this.setState({
-            currentUser: {
-              id: snapshot.id,
-              ...snapshot.data()
-            }
-          }, 
-          //because setstate asyncronous, we need to console.log as another param to setstate, so we are waiting for the state to be set before logging
-          () => {
-            // console.log(this.state)
-
+          setCurrentUser({
+            id: snapshot.id,
+            ...snapshot.data()
           })
         })
       } else {
         //otherwise, this will set the user's state to null, which we get back from the auth library
-        this.setState({ currentUser: userAuth })
+        setCurrentUser(userAuth)
         console.log(this.state)
 
       }
@@ -54,7 +45,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
@@ -64,4 +55,9 @@ class App extends React.Component {
     );
   }
 }
-export default App;
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import { connect } from 'react-redux'
 import { auth } from '../../firebase/firebase.utils'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import './header.component.styles.scss'
 
-export default function Header({ currentUser }) {
+const Header = ({ currentUser }) => {
   return (
     <div className='header'>
       <Link className='logo-container' to='/'>
@@ -27,3 +28,9 @@ export default function Header({ currentUser }) {
     </div>
   )
 }
+
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header)
